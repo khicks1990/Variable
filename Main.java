75a48abe7a98d668fd5adc731c65b234b3cff399
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-19 08:49:22 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.logging;
+
+// This program has a variable.
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+      int value;
+
+      value = 45;
+      System.out.print("The value is ");
+      System.out.println(value);
+   
+   }
+}
